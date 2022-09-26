886850f3034717253132d98796de7bb57b4c87aa
eternalmangekyo022 <tibcso0322@gmail.com>
2022-09-26 16:52:39 +0200

    added tate head

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './index.css';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 
 function App(): JSX.Element {
+    const [drawer, setDrawer] = useState<boolean>(false);
 
     return <>
         <div className='relative h-screen w-screen'>
-          
+            <header className='absolute w-full h-16 bg-black'>
+                <button /*navigation button*/
+                    className='w-10 bg-white aspect-square'
+                ></button>
+            </header>
         </div>
     </>;
 
