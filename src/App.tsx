f5a0db078b4379ee39fdecab35a961b72cb8b230
eternalmangekyo022 <tibcso0322@gmail.com>
2022-09-30 21:59:11 +0200

    fixed anchor tag problem

--- src/App.tsx
+++ src/App.tsx
@@ -48,12 +48,12 @@ function App(): JSX.Element {
                                 ['About me', './about', 'https://www.svgrepo.com/show/257694/question.svg'],
                             ].map((i, index) => <>
                                 <motion.li
-                                    className='m-5 flex w-[65vw] h-8 cursor-pointer bg-neutral-500 justify-center items-center rounded-lg hover:bg-neutral-400 transition-colors'
+                                    className='m-5 flex w-[65vw] h-8 cursor-pointer bg-neutral-500 text-center justify-center items-center rounded-lg hover:bg-neutral-400 transition-colors'
                                     initial={{ scale: 0, x: -1000 }}
                                     animate={{ scale: 1, x: 0 }}
                                     transition={{ delay: .05 + (index / 10) }}
                                 >
-                                    <a href={i[1]}>{i[0]} <img width='20' className='inline ml-1 mb-1' src={i[2]}></img></a>
+                                    <a target='_blank' href={i[1]} rel="noreferrer" className='w-full'>{i[0]} <img width='20' className='inline ml-1 mb-1' src={i[2]}></img></a>
                                 </motion.li>
                             </>)}
                         </ul>
